4f5c0762c67817a4ceca195a097974be4132dc26
fliepeltje <donatas@rasiukevicius.dev>
2020-07-16 17:39:40 +0200

    Rewrite example test to test db connection

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,6 +1,8 @@
 use ch3f_utils::db;
+use std::env;
 
 #[test]
-fn example() {
-    assert_eq!(4, 1 + 3);
+fn db_connection_thrwos_error_if_env_var_not_defined() {
+    env::remove_var(db::ENV_DB_PATH);
+    assert!(db::establish_connection().is_err());
 }
